1dead9d41a1909b612284157c6d6eb0cc397cc0b
Johnathon47 <hei.johnathon.2@gmail.com>
2025-11-30 20:28:57 +0300

    chore: add manager of location

---
+++ src/main/java/org/wesley/manager/LocationManager.java
@@ -0,0 +1,9 @@
+package org.wesley.manager;
+
+import org.wesley.entity.Locationable;
+
+public class LocationManager {
+    public boolean checkAvailability(Locationable item) {
+        return true;
+    }
+}
