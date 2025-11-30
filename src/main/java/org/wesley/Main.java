a5c45da343667e8c58243bf1ef510abc161f5dc3
Johnathon47 <hei.johnathon.2@gmail.com>
2025-11-30 20:32:43 +0300

    chore: add main for testing if it works

---
+++ src/main/java/org/wesley/Main.java
@@ -0,0 +1,20 @@
+package org.wesley;
+
+import org.wesley.controller.LocationController;
+import org.wesley.entity.Car;
+import org.wesley.entity.Locationable;
+import org.wesley.entity.Locationneurable;
+import org.wesley.entity.Person;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        LocationController controller = new LocationController();
+
+        Locationneurable client = new Person("Wesley");
+        Locationable item = new Car(1000.0);
+
+        controller.process(client, item);
+    }
+}
