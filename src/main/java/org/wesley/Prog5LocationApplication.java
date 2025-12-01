f18103fc3307e94658ef9c1a5be2d41d94ea0ccc
Johnathon47 <hei.johnathon.2@gmail.com>
2025-12-01 08:09:04 +0300

    feat: change name of file and adjust to springboot

--- src/main/java/org/wesley/Prog5LocationApplication.java
+++ src/main/java/org/wesley/Prog5LocationApplication.java
@@ -1,20 +1,11 @@
 package org.wesley;
 
-import org.wesley.controller.LocationController;
-import org.wesley.entity.Car;
-import org.wesley.entity.Locationable;
-import org.wesley.entity.Locationneurable;
-import org.wesley.entity.Person;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+@SpringBootApplication
+public class Prog5LocationApplication {
     public static void main(String[] args) {
-        final LocationController controller = new LocationController();
-
-        final Locationneurable client = new Person("Wesley");
-        final Locationable item = new Car(1000.0);
-
-        controller.process(client, item);
+        SpringApplication.run(Prog5LocationApplication.class, args);
     }
 }
