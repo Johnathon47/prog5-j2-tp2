ae011227547aa77bb9308aaa97caa4057698950f
Johnathon47 <hei.johnathon.2@gmail.com>
2025-12-01 08:09:46 +0300

    feat: change controller to available in web

--- src/main/java/org/wesley/controller/LocationController.java
+++ src/main/java/org/wesley/controller/LocationController.java
@@ -1,15 +1,28 @@
 package org.wesley.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.wesley.entity.Car;
 import org.wesley.entity.Locationable;
 import org.wesley.entity.Locationneurable;
+import org.wesley.entity.Person;
 import org.wesley.manager.LocationManager;
 import org.wesley.services.LocationService;
 
+@RestController
 public class LocationController {
 
     private LocationService service =  new LocationService();
     private LocationManager manager =  new LocationManager();
 
+    @GetMapping("rent/test")
+    public String rentTest() {
+        final Locationneurable client = new Person("Wesley");
+        final Locationable car = new Car(1000.0);
+
+        service.rentItem(client, car);
+        return "rent success";
+    }
     public void process(Locationneurable client, Locationable item) {
         if (manager.checkAvailability(item)) {
             service.rentItem(client, item);
